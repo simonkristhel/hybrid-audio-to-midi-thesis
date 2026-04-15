52a017939d847fad80ae67ecee35f31170fc7d70
Kristhel Simon <kristhelsimon08@gmail.com>
2026-04-15 22:48:47 +0800

    Remove obsolete manual test files

--- test_crepe.py
+++
@@ -1,8 +0,0 @@
-from modules.crepe_module import extract_crepe_pitch
-
-pitch_df = extract_crepe_pitch(
-    audio_path="data/separated_audio/test/vocals.wav"
-)
-
-print(pitch_df.head())
-print("\nTotal frames:", len(pitch_df))
--- test_spleeter.py
+++
@@ -1,8 +0,0 @@
-from modules.spleeter_module import separate_monophonic_audio
-
-output = separate_monophonic_audio(
-    input_audio_path="data/raw_audio/test.wav",
-    output_dir="data/separated_audio"
-)
-
-print("Separated file saved at:", output)
